1a96a89ab89954c92696e2d21d0def48b73dcf4c
migbro <miguel.a.brown@gmail.com>
2016-07-15 16:25:24 -0500

    debug

--- get_cinder_usage.py
+++ get_cinder_usage.py
@@ -27,5 +27,5 @@ tot_use = 0
 for i in xrange(3, (len(cur) - 2), 1):
     info = messy_string2list(cur[i])
     tot_use += int(info[3])
-    sys.stdout.write('\t'.join((info[0], info[2], info[3], info[6])) + '\n')
+    sys.stdout.write('\t'.join((info[0], info[1], info[2], info[3], info[6])) + '\n')
 sys.stdout.write('TOTAL\tNA\tNA\t' + str(tot_use) + '\tNA\n')
